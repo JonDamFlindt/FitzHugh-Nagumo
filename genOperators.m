fc8c62dd8eb7c2886b5799505d44fe983bcd861f
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2024-12-17 12:43:26 +0100

    Update genOperators.m

--- genOperators.m
+++ genOperators.m
@@ -1,4 +1,4 @@
-function [E, A, c] = genOperators(N)
+function [h, e, A, c] = genOperators(N)
 
 L = 1; % length
 e = 0.015;
@@ -7,14 +7,12 @@ gamma = 2;
 c = 0.05;
 h = L/(N + 2);
 
-E = ones(2*N,1);
-E(1:N) = e*E(1:N);
-% Caution: this E does not have the factor (1/dt) !!
-K = toeplitz([-2 1 zeros(1, N - 2)]);
-K(1, 1) = -1;
-K(end, end) = -1;
-K = -K;
-A = [-e^2/h^2*K, -eye(N); b*eye(N), -gamma*eye(N)];
+Dxx = toeplitz([-2 1 zeros(1, N - 2)]);
+Dxx(1, 1) = -1;
+Dxx(end, end) = -1;
+Dxx = (1/h^2)*Dxx;
+A = [e*Dxx, (-1/e)*eye(N); b*eye(N), -gamma*eye(N)];
 c = c*ones(2*N, 1);
+c(1:N) = (1/e)*c(1:N);
 return;
 end
